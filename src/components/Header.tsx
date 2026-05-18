b77be095a3df1b0db7d55ac19543f9d4bd248050
Ivan Kiral <kiralivan@gmail.com>
2026-05-18 16:45:57 +0200

    hide language switcher in preview mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,21 @@
+import { draftMode } from "next/headers";
 import type { FC } from "react";
 import { Container } from "./Container.tsx";
 import { LanguageSwitcher } from "./LanguageSwitcher.tsx";
 import { Logo } from "./Logo.tsx";
 import { Navigation } from "./Navigation.tsx";
 
-export const Header: FC = () => (
-  <Container>
-    <div className="py-8 flex flex-col gap-8 xl:gap-0 lg:flex-row items-center justify-between">
-      <div className="flex flex-col lg:flex-row gap-5 lg:gap-12 xl:gap-32 items-center list-none">
-        <Logo />
-        <Navigation />
+export const Header: FC = async () => {
+  const { isEnabled: isPreviewEnabled } = await draftMode();
+  return (
+    <Container>
+      <div className="py-8 flex flex-col gap-8 xl:gap-0 lg:flex-row items-center justify-between">
+        <div className="flex flex-col lg:flex-row gap-5 lg:gap-12 xl:gap-32 items-center list-none">
+          <Logo />
+          <Navigation />
+        </div>
+        {!isPreviewEnabled && <LanguageSwitcher />}
       </div>
-      <LanguageSwitcher />
-    </div>
-  </Container>
-);
+    </Container>
+  );
+};
